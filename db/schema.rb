a615fda83efd2122312aad6e72be99df29b9d0b2
geekgrl1965 <geekgrl1@hotmail.com>
2017-10-21 16:00:23 -0400

    back end changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021084949) do
+ActiveRecord::Schema.define(version: 20171021165205) do
 
   create_table "birds", force: :cascade do |t|
     t.string "species"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20171021084949) do
     t.string "birder"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
