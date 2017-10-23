a9bfe9f3338fdb86c369950dab887d0196380a11
geekgrl1965 <geekgrl1@hotmail.com>
2017-10-23 16:40:19 -0400

    preparing to merge into master

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :birds
-  resources :users
 
- 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :birds, only: [:index]
+resources :users do
+resources :birds
+
+ end
 end
